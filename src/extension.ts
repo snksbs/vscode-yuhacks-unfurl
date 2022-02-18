96c37a44b0c5aa851f1d44b52fe168bc1ff11041
Yousif <haqi.yousef@gmail.com>
2022-02-18 15:45:14 -0500

    Added interface for snippet objects

--- src/extension.ts
+++ src/extension.ts
@@ -2,10 +2,20 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+
+interface IUnfurlSnip {
+	label: string;
+	description?: string;
+	snippet: string;
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
+
+	let unfurlSnips: IUnfurlSnip[] = [];
+
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vscode-yuhacks-unfurl" is now active!');
@@ -23,4 +33,4 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
